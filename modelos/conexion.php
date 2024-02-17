acc7f363fa036c74571071141e3d0ca3d301b129
JorgeTorres <jorgetorres@fwdlogistica.com>
2024-02-17 17:57:23 -0600

    cambiar conexion BD

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,15 +1,18 @@
 <?php
+class Conexion {
+    static public function conectar() {
+        $host = "localhost"; // Reemplaza con el nombre del servidor de MySQL proporcionado por Hostinger
+        $dbname = "u912617504_inventario"; // Reemplaza con el nombre de tu base de datos
+        $username = "u912617504_inventario"; // Reemplaza con tu nombre de usuario de MySQL en Hostinger
+        $password = "Stihlmiranchito2024!"; // Reemplaza con tu contraseña de MySQL en Hostinger
 
-class Conexion{
-
-	static public function conectar(){
-		
-		$link = new PDO("mysql:host=localhost;dbname=inventario", "root", "");
-
-		$link->exec("set names utf8");
-
-		return $link;
-
-	}
-
+        try {
+            $link = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $link->exec("set names utf8");
+            return $link;
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+            die();
+        }
+    }
 }
